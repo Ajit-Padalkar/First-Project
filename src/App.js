214f9f90bde0531ed3b25c28ce72235a42dcd190
Ajit9090 <ajitpadalkar2001@gmail.com>
2021-08-19 16:28:00 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Ajirt from './components/Ajirt';
+
+function App() {
+  return (
+    <div className="App">
+      <div >
+      <Ajirt />
+      <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Ajirt.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Ajirt() {
+    return (
+        <div>
+            Ajit
+        </div>
+    )
+}
+
+export default Ajirt
